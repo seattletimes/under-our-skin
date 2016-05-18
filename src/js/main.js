cd9e59322879a5bb426e3aa487be7fa1ea7db7f8
Audrey Carlsen <acarlsen@news-0960.seatimes.com>
2016-05-18 16:12:01 -0700

    emit playVideo and updatePlaylist events

--- src/js/main.js
+++ src/js/main.js
@@ -5,11 +5,23 @@ var $ = require("./lib/qsa");
 var ready = require("./brightcove");
 require("./form");
 
+var EventEmitter = require("events");
+var channel = new EventEmitter();
+
+var videoLookup = {};
+playlistItems.forEach(i => videoLookup[i.video_id] = i);
+
 var pageIndex = 0;
 var sections = $(".flex-container");
 var introPlayer = null;
 var videoPlayer = null;
 
+var pending = null;
+var preLoaded = v => pending = v;
+channel.on("playVideo", preLoaded);
+
+
+// Page transitions
 var fadeIn = function(el) {
   el.classList.add("shown");
   var reflow = el.offsetWidth;
@@ -25,6 +37,7 @@ var fadeOut = function(el) {
 var show = (el) => el.classList.add("fade", "shown");
 var hide = (el) => el.classList.remove("fade", "shown");
 
+// Navigate to page
 var navigateTo = function(index, silent) {
   sections.forEach(function(section) {
     if (index == section.getAttribute("data-index")) {
@@ -40,6 +53,8 @@ var navigateTo = function(index, silent) {
   if (introPlayer) {
     if (index == 1) {
       introPlayer.play();
+    } else {
+      introPlayer.pause();
     }
   }
   if (videoPlayer) {
@@ -50,64 +65,92 @@ var navigateTo = function(index, silent) {
   }
 };
 
+// Insert video title, comments, and URL hash
+var loadVideoInfo = function(v) {
+  var playlistItem = document.querySelector(`[data-id="${v.video_id}"]`);
+  console.log(playlistItem)
+  var playing = document.querySelector(".playlist-video.playing");
+  if (playing) playing.classList.remove("playing");
+  playlistItem.classList.add("playing");
+  window.location.hash = v.term;
+  document.querySelector(".title.tile").innerHTML = `"${v.word}"`;
+  $(".comment").forEach(function(comment){
+    if (comment.getAttribute("data-term") == v.term) {
+      comment.classList.add("visible");
+    } else {
+      comment.classList.remove("visible");
+    }
+  })
+}
+
+channel.on("updatePlaylist", loadVideoInfo);
+
+// If URL already contains video hash, load correct video
 var term = window.location.hash.replace("#", "");
 if (term) {
   navigateTo(3, true);
+  pending = playlistItems.filter(v => v.term == term).pop();
 }
 
+// Load intro video player
 ready("default", "intro-player", p => introPlayer = p);
 
+// Set up event listeners
 document.body.addEventListener("click", function(e) {
+
+  // Next buttons
   if (e.target.classList.contains("next")) {
     pageIndex = e.target.getAttribute("data-index");
     navigateTo(pageIndex);
   };
+
+  // Read more button
   if (e.target.classList.contains("read-more")) {
     e.target.classList.add("hide");
     fadeIn(document.querySelector(".editors-note .more"));
   };
+
+  // Playlist functionality
+  if (e.target.classList.contains("playlist-video")) {
+    var id = e.target.getAttribute("data-id");
+    var v = videoLookup[id];
+    channel.emit("playVideo", v);
+    channel.emit("updatePlaylist", v);
+  };
 });
 
-// Video player
 
+// Load words video player
 var playlistID = 4884471259001;
 
-// word video player
 ready("B15NOtCZ", "player", function(player) {
   window.player = videoPlayer = player;
 
-  var loadVideoInfo = function(term, label) {
-    window.location.hash = term;
-    document.querySelector(".title.tile").innerHTML = `"${label}"`;
-    $(".comment").forEach(function(comment){
-      if (comment.getAttribute("data-term") == term) {
-        comment.classList.add("visible");
-      } else {
-        comment.classList.remove("visible");
-      }
-    })
-  }
-
   player.on("loadedmetadata", function() {
     var id = player.mediainfo.id ;
-    var v = playlistItems.filter(p => p.video_id == id).pop();
-    loadVideoInfo(v.term, v.word);
+    var v = videoLookup[id];
+    channel.emit("updatePlaylist", v);
   });
 
   var cuePlaylist = function(index, term, label) {
     navigateTo(3);
     player.playlist.currentItem(index);
     player.play();
-    loadVideoInfo(term, label);
   }
 
   player.catalog.getPlaylist(playlistID, function(err, playlist) {
     player.catalog.load(playlist);
 
-    var term = window.location.hash.replace("#", "");
-    if (term) {
-      var v = playlistItems.filter(p => p.term == term).pop();
-      cuePlaylist(v.index, v.term, v.word);
+    channel.removeListener("playVideo", preLoaded);
+
+    channel.on("playVideo", function(v) {
+      player.playlist.currentItem(v.index);
+      player.play();
+    });
+
+    if (pending) {
+      player.playlist.currentItem(pending.index);
+      player.play();
     }
 
     $(".word.tile").forEach(tile => tile.addEventListener("click", function(e) {
