fb9a7219f03412a159b6833c16dc000d0389eea2
Audrey Carlsen <acarlsen@seattletimes.com>
2016-06-02 12:52:18 -0700

    fixes comment form validation errors

--- src/js/form.js
+++ src/js/form.js
@@ -11,16 +11,14 @@ var inputs = form.find("input, select, textarea");
 var textarea = form.find("textarea");
 var select = form.find("select");
 var charLimit = $(".char-limit");
+var input = document.querySelector(".adjective input");
 var validated = false;
 
 select.on("change", function() {
   if (select.val() == "other") {
-    document.querySelector(".adjective").innerHTML = `
-      <select name="adjective">
-        <input maxlength="100" name="adjective" placeholder="" required>
-      </select> 
-    `;
-    document.querySelector(".adjective input").classList.add("visible");
+    select.addClass("hidden");
+    input.value = "";
+    input.classList.add("visible");
   }
 })
 
@@ -68,25 +66,15 @@ submit.on("click", function(e) {
     messageSuccess.addClass("visible");
     charLimit.html("500");
     validated = false;
-    document.querySelector(".adjective").innerHTML = `
-      <select name="adjective">
-        <option value="choose" selected="selected" disabled="disabled">CHOOSE</option>
-        <option value="surprised">surprised</option>
-        <option value="interested">interested</option>
-        <option value="inspired">inspired</option>
-        <option value="resonated">resonated with</option>
-        <option value="confused">confused</option>
-        <option value="frustrated">frustrated</option>
-        <option value="saddened">saddened</option>
-        <option value="angered">angered</option>
-        <option value="other">(write your own)</option>
-        <input maxlength="100" name="adjective" placeholder="" required>
-      </select> 
-    `;
+    select.removeClass("hidden");
+    input.classList.remove("visible");
     form.removeClass("validated");
     inputs.each(function(i, el) {
-      if (el.name == "adjective") {
+      console.log(el.name)
+      if (el.name == "select-adjective") {
         el.value = "choose";
+      } else if (el.name == "input-adjective") {
+        el.value = "--";
       } else {
         el.value = "";
       }
